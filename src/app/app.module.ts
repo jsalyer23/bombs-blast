03b154ca52a5e8c781ad8abd018567de15ad8b0c
Josh Salyer <joshsalyer23@gmail.com>
2019-01-22 13:18:30 -0600

    Adds ToolbarComponent, AboutComponent, ContactComponent, HomeComponent, MediaComponent, and TourComponent to main AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { TourComponent } from './tour/tour.component';
+import { MediaComponent } from './media/media.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HomeComponent,
+    AboutComponent,
+    TourComponent,
+    MediaComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
