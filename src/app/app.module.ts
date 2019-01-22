8e9075251dd1622a7efb812c7fde63035e58ce0b
Josh Salyer <joshsalyer23@gmail.com>
2019-01-22 17:04:19 -0600

    Adds footer and imports for FooterComponent to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AboutComponent } from './about/about.component';
 import { TourComponent } from './tour/tour.component';
 import { MediaComponent } from './media/media.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ContactComponent } from './contact/contact.component';
     AboutComponent,
     TourComponent,
     MediaComponent,
-    ContactComponent
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
