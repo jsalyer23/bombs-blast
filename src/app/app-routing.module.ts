4c1c36f8b29d544f1ecc17ae25e3e4ef4d37f252
Josh Salyer <joshsalyer23@gmail.com>
2019-01-22 13:13:55 -0600

    Adds routing module with about, contact, home, media, and tour paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MediaComponent } from './media/media.component';
+import { ContactComponent } from './contact/contact.component';
+import { TourComponent } from './tour/tour.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: []
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    children: []
+  },
+  {
+    path: 'media',
+    component: MediaComponent,
+    children: []
+  },
+  {
+    path: 'contact-us',
+    component: ContactComponent,
+    children: []
+  },
+  {
+    path: 'tour',
+    component: TourComponent,
+    children: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
