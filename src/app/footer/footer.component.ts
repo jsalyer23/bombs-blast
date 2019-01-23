b152b8a9643ac7fe3d81a4e58d1c21d6ae6e536c
Josh Salyer <joshsalyer23@gmail.com>
2019-01-22 18:04:21 -0600

    Adds footer component, component styles, and facebook link

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  public facebookUrl: String = 'https://www.facebook.com/bombsblast/';
+
   constructor() { }
 
   ngOnInit() {
