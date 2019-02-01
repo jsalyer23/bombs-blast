4ff88fee966f311eb759d66dc7553ba54e3026f3
Josh Salyer <joshsalyer23@gmail.com>
2019-02-01 15:56:42 -0600

    Updated home component to use image input property

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentCardComponent } from '../content-card/content-card.component';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  public title: string = 'Bombs Blast';
+  public content: string = 'Content goes here';
 
   constructor() { }
 
