df2d9ba7b99f7065b82d9ff125ff2e1c93803012
Josh Salyer <joshsalyer23@gmail.com>
2019-02-01 16:31:04 -0600

    Adds band information into object, sets up base for about page, adds styles

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentCardComponent } from '../content-card/content-card.component';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+
+  public title: string = 'About Bombs Blast';
+  public bombsBlast: Object = {
+    genre: 'Punk, Ska, Hardcore',
+    bandMembers: [
+      { name: 'Zach', role: 'Vocals' },
+      { name: 'Kaden', role: 'Guitar' },
+      { name: 'Jarett', role: 'Drums' },
+      { name: 'Tony', role: 'Bass' },
+      { name: 'Danny', role: 'Guitar' }
+    ],
+    homeTown: 'Kearney, NE',
+    recordLabel: { name: 'Hellcat Records', url: 'www.hell-cat.com' },
+    biography: `Started in 2007 Bombs Blast has been traveling almost non-stop week to week playing our brand of dirty spit in yer face ska-punk...We've played everything from back yards to Auditoriums...Venues to parks to bars to dingy little basements... Too strong to quit, we're here to stay...takin what's ours and making our name!`,
+    currentLocation: 'Kearney, NE',
+    influences: 'Rancid, Choking Victim, The Unseen, Cheap Sex',
+    contact: 'bombsblastofficial@gmail.com'
+  };
 
   constructor() { }
 
